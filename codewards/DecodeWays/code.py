6eef1b3c95d8f2d35dbadbacca0035ab920f5471
KameniAlexNea <Kamenialexnea@gmail.com>
2022-10-23 00:21:48 +0200

    decode ways implementation ans tests

---
+++ codewards/DecodeWays/code.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        computed = {}
+        def numDecodingsRec(x):
+            print(x)
+            if len(x) == 0:
+                return 1
+            if x[0] == '0':
+                return 0
+            if len(x) < 3:
+                if x[-1] == '0':
+                    return 1 if int(x) < 27 else 0
+                return 2 if (int(x) < 27 and len(x) == 2) else 1
+            l = 0
+            if x[1:] not in computed:
+                l = numDecodingsRec(x[1:])
+                print(x[1:], l)
+            else:
+                l = computed[x[1:]]
+            if x[2:] not in computed:
+                if int(x[:2]) < 27:
+                    l += numDecodingsRec(x[2:])
+                print(x[2:], l)
+            else:
+                if int(x[:2]) < 27:
+                    l += computed[x[2:]]
+            computed[x] = l
+            return l
+        return numDecodingsRec(s)
---
+++ tests/test_decode_ways.py
@@ -0,0 +1,5 @@
+from codewards.DecodeWays.code import Solution
+
+
+def test_decode_ways():
+    assert Solution().numDecodings("123123") == 9
