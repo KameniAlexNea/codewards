16eedd8fe8d22e33ab8d66539b734ef659fc9027
KameniAlexNea <45461704+KameniAlexNea@users.noreply.github.com>
2022-10-30 22:09:35 +0100

    Time: 101 ms (18.79%), Space: 14.5 MB (43.59%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def searchListRec(self, line: List[int], left, right, target):
+        if right <= left+1:
+            return (line[left] == target) or (line[right] == target)
+        mid = (left + right) // 2
+        elm = line[mid]
+        if elm == target:
+            return True
+        elif elm < target:
+            return self.searchListRec(line, mid, right, target)
+        return self.searchListRec(line, left, mid, target)
+        
+        
+    def searchMatrixRec(self, matrix: List[List[int]], left, right, target):
+        if right <= left+1:
+            line = matrix[right]
+            if line[0] == target:
+                return True
+            elif line[0] < target:
+                return self.searchListRec(line, 0, len(line) - 1, target)
+            line = matrix[left]
+            return self.searchListRec(line, 0, len(line) - 1, target)
+        
+        mid = (left + right) // 2
+        elm = matrix[mid][0]
+        
+        if elm == target:
+            return True
+        elif elm < target:
+            return self.searchMatrixRec(matrix, mid, right, target)
+        return self.searchMatrixRec(matrix, left, mid, target)
+    
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        return self.searchMatrixRec(matrix, 0, len(matrix)-1, target)
