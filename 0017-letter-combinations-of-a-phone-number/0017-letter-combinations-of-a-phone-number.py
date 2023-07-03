b20ea856608abd1db579ef6b12e73f4ef5b3fd77
KameniAlexNea <45461704+KameniAlexNea@users.noreply.github.com>
2023-07-04 00:27:51 +0200

    Time: 38 ms (94.18%), Space: 16.6 MB (17.05%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,4 +1,32 @@
-class Solution:
+def combine_gpt(digits: str):
+    if not digits:
+        return []
+
+    mapping = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z']
+    }
+
+    result = []
+
+    def generate_combinations(combination, remaining_digits):
+        if not remaining_digits:
+            result.append(combination)
+        else:
+            for letter in mapping[remaining_digits[0]]:
+                generate_combinations(combination + letter, remaining_digits[1:])
+
+    generate_combinations('', digits)
+    return result
+
+def combine_alex(digits: str):
+    solution = []
     letters = {
         "2": "abc",
         "3": "def",
@@ -9,19 +37,19 @@ class Solution:
         "8": "tuv",
         "9": "wxyz"
     }
-
-    def combiner(self, nbers: str, data: str, solution: list):
+    def combiner(self, nbers: str, data: str):
         if len(nbers) == 0:
             return solution.append(data)
-        for i in self.letters[nbers[0]]:
-            self.combiner(nbers[1:], data+i, solution)
-    
+        for i in letters[nbers[0]]:
+            combiner(nbers[1:], data+i)
+    if len(digits) == 0:
+        return []
+    if len(digits) == 1:
+        return list(letters[digits], solution)
+    combiner(digits, "")
+    return solution
+
+class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
-        solution = []
-        if len(digits) == 0:
-            return []
-        if len(digits) == 1:
-            return list(self.letters[digits])
-        self.combiner(digits, "", solution)
-        result = set(solution)
+        result = set(combine_gpt(digits))
         return result
