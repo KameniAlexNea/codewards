eeb510af0d869aea80acbb67c20d704f3c2859c7
KameniAlexNea <45461704+KameniAlexNea@users.noreply.github.com>
2024-03-13 18:32:58 +0100

    Time: 172 ms (19.11%), Space: 17.9 MB (79.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,19 @@
-import collections
-
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        result = collections.defaultdict(int)
+    def majorityElement(self, nums: list[int]) -> int:
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        
+        return candidate
+    def majorityElementI(self, nums: list[int]) -> int:
+        result = dict()
         n = len(nums) / 2
         for i in nums:
-            result[i] += 1
+            result[i] = result.get(i, 0) + 1
         for k, v in result.items():
             if v >= n:
                 return k
