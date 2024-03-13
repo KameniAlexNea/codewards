a757ec19161cbd333137768c9d75e1da55ed2378
KameniAlexNea <45461704+KameniAlexNea@users.noreply.github.com>
2024-03-13 18:25:42 +0100

    Time: 178 ms (11.37%), Space: 18.1 MB (39.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,10 +2,10 @@ import collections
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        result = dict()
+        result = collections.defaultdict(int)
         n = len(nums) / 2
         for i in nums:
-            result[i] = result.get(i, 0) + 1
+            result[i] += 1
         for k, v in result.items():
             if v >= n:
                 return k
