4097ccdf6f7f869cd2bf3a3b10e503c1ad92bb5d
KameniAlexNea <45461704+KameniAlexNea@users.noreply.github.com>
2024-03-19 20:40:13 +0100

    Time: 1117 ms (5.18%), Space: 16.6 MB (40.58%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -2,5 +2,5 @@ import re
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        regex = ".*".join(list(s))
-        return len(re.findall(regex, t)) > 0
+        regex = ".*" + (".*".join(list(s))) + ".*"
+        return re.match(regex, t) is not None
