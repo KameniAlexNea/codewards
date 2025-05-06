09d343512a039166d12e52062c195143d63517d9
KameniAlexNea <45461704+KameniAlexNea@users.noreply.github.com>
2025-05-06 17:31:58 +0200

    Time: 48 ms (8.67%) | Memory: 18 MB (13.85%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -1,9 +1,7 @@
 class Solution:
     def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
-        if fx == sx and fy == sy:
-            return t != 1
-        return max(abs(fx - sx), abs(fy - sy)) <= t
-        # compute minimum time to move from (sx, sy) -> (fx, fy)
 
-        diag = min(abs(fx - sx), abs(fy - sy))
-        vert = max(abs(fx - sx), abs(fy - sy)) - diag
+        return (fx == sx and fy == sy and t != 1) or (
+            (fx != sx or fy != sy) and
+            (max(abs(fx - sx), abs(fy - sy)) <= t)
+        )
